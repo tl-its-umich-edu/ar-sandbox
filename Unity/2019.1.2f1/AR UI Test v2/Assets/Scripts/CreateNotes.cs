cc926c218cfd3d58a76efd09725b96437bbe2e4d
Alex Kim <akim8@emich.edu>
2019-05-16 12:21:28 -0400

    Add AR UI Test project

---
+++ Unity/2019.1.2f1/AR UI Test v2/Assets/Scripts/CreateNotes.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class CreateNotes : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+    public TMP_InputField inputField;
+    public Button placeButton, deleteButton;
+
+    private ARRaycastManager arRaycastManager;
+    private Pose placementPose;
+    private bool placementPoseIsValid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        placeButton.onClick.AddListener(PlaceNote);
+        deleteButton.onClick.AddListener(DeleteNote);
+
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void PlaceNote()
+    {
+        GameObject newNote = Instantiate(objectToPlace, placementPose.position, placementPose.rotation) as GameObject;
+        NoteBehavior newNoteScript = newNote.GetComponent<NoteBehavior>();
+        newNoteScript.changeNoteText(inputField.text);
+        inputField.text = "";
+    }
+
+    private void DeleteNote()
+    {
+
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        arRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraForward = Camera.main.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+
+            placeButton.interactable = true;
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+
+            placeButton.interactable = true;
+        }
+    }
+}
---
+++ Unity/2019.1.2f1/AR UI Test v2/Assets/Scripts/NoteBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class NoteBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeNoteText(string newText)
+    {
+        TMP_Text noteTextBox = FindObjectOfType<TMP_Text>();
+        noteTextBox.text = newText;
+    }
+}
