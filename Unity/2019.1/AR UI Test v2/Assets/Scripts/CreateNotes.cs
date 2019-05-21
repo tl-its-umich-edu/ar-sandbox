2bf781c0c5b5a86225c79507bffe1ef1f9e13932
Alex Kim <akim8@emich.edu>
2019-05-21 14:05:19 -0400

    note size slider, QOL improvements
    also reorganized projects by unity minor version instead of patch version

---
+++ Unity/2019.1/AR UI Test v2/Assets/Scripts/CreateNotes.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class CreateNotes : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    public GameObject objectToPlace;
+    public TMP_InputField inputField;
+    public Slider noteSizeSlider;
+    public Button placeButton, deleteButton;
+
+    private ARRaycastManager arRaycastManager;
+    private ARPlaneManager arPlaneManager;
+    private Pose placementPose;
+    private bool placementPoseIsValid;
+    
+    private RaycastHit deleteRayHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        placeButton.onClick.AddListener(PlaceNote);
+        deleteButton.onClick.AddListener(DeleteNote);
+
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+        arPlaneManager = FindObjectOfType<ARPlaneManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        UpdatePlaceButton();
+        UpdateDeleteButton();
+    }
+
+    private void PlaceNote()
+    {
+        GameObject newNote = Instantiate(objectToPlace, placementPose.position, placementPose.rotation) as GameObject;
+        newNote.transform.localScale *= noteSizeSlider.value;
+
+        NoteBehavior newNoteScript = newNote.GetComponent<NoteBehavior>();
+        newNoteScript.changeNoteText(inputField.text);
+        inputField.text = "";
+    }
+
+    private void DeleteNote()
+    {
+        Destroy(deleteRayHit.transform.gameObject);
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        arRaycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinBounds);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            if (placementPose.rotation.eulerAngles.x < 10) // if looking at horizontal plane
+            {
+                var cameraForward = Camera.main.transform.forward;
+                var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+
+                placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+            }          
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+            placementIndicator.transform.localScale = Vector3.one * noteSizeSlider.value;
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlaceButton()
+    {
+        if (placementPoseIsValid && inputField.text != "")
+        {
+            placeButton.interactable = true;
+        }
+        else
+        {
+            placeButton.interactable = false;
+        }
+    }
+
+    private void UpdateDeleteButton()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out deleteRayHit, Mathf.Infinity)) // also sets deleteRayHit
+        {
+            deleteButton.interactable = true;
+        }
+        else
+        {
+            deleteButton.interactable = false;
+        }
+    }
+}
---
+++ Unity/2019.1/Pokedex/Assets/Scripts/TrackedImageManipulator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+
+public class TrackedImageManipulator : MonoBehaviour
+{
+    ARTrackedImageManager trackedImageManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        trackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(trackedImageManager.trackables.count);
+    }
+}
