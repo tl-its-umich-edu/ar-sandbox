0c4683db95e6d2d4b3b639a99d35b4b744d813c0
Alex Kim <akim8@emich.edu>
2019-05-22 16:30:05 -0400

    image recognition with mcard and door sign

--- Unity/2019.1/Pokedex/Assets/Scripts/TrackedImageManipulator.cs
+++ Unity/2019.1/Pokedex/Assets/Scripts/TrackedImageManipulator.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,10 +7,19 @@ using UnityEngine.XR.ARFoundation;
 
 public class TrackedImageManipulator : MonoBehaviour
 {
-    ARTrackedImageManager trackedImageManager;
+    public GameObject mustachePrefab;
+    public GameObject doorNotesPrefab;
+
+    private ARTrackedImageManager trackedImageManager;
 
     // Start is called before the first frame update
     void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
 
     }
@@ -20,9 +29,42 @@ public class TrackedImageManipulator : MonoBehaviour
         trackedImageManager = GetComponent<ARTrackedImageManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        Debug.Log(trackedImageManager.trackables.count);
+        trackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        trackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            Debug.Log(trackedImage.referenceImage.name);
+            switch (trackedImage.referenceImage.name)
+            {
+                case "mcard":
+                    GameObject mustache = Instantiate(mustachePrefab, trackedImage.transform.position, trackedImage.transform.rotation) as GameObject;
+                    mustache.transform.parent = trackedImage.transform;
+                    break;
+                case "210":
+                    GameObject doorNotesInst = Instantiate(doorNotesPrefab, trackedImage.transform.position, trackedImage.transform.rotation) as GameObject;
+                    doorNotesInst.transform.parent = trackedImage.transform;
+                    break;
+            }
+        }
+
+        foreach (var trackedImage in eventArgs.updated)
+        {
+
+        }
+
+        foreach (var trackedImage in eventArgs.removed)
+        {
+            Destroy(trackedImage);
+        }
     }
 }
