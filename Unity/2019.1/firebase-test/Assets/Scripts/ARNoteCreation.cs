9c696d2e45e0ec39e3ef09b2cce04538240d837f
Alex Kim <akim8@emich.edu>
2019-07-22 15:22:12 -0400

    commit start of poster project
    can place notes

---
+++ Unity/2019.1/firebase-test/Assets/Scripts/ARNoteCreation.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ARNoteCreation : MonoBehaviour
+{
+    public Button placeButton;
+    public TMP_InputField noteTextField;
+    public GameObject placementIndicator, notePrefab;
+
+    private float lerpSpeed = .5f;
+
+    private SurfaceDetection surfaceDetection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        surfaceDetection = GetComponent<SurfaceDetection>();
+
+        placeButton.onClick.AddListener(placeButtonEvent);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateButtons();
+        UpdatePlacementPose();
+    }
+
+    private void placeButtonEvent() // place note object
+    {
+        PlaceNote(noteTextField.text, placementIndicator.transform.position, placementIndicator.transform.rotation);
+        noteTextField.text = "";
+    }
+
+    private void UpdateButtons()
+    {
+        if (surfaceDetection.GetPlacementPoseIsValid() && noteTextField.text != "")
+        {
+            placeButton.interactable = true;
+        }
+        else
+        {
+            placeButton.interactable = false;
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        if (surfaceDetection.GetPlacementPoseIsValid())
+        {
+            var placementPose = surfaceDetection.GetPlacementPose();
+
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, Quaternion.Lerp(placementIndicator.transform.rotation, placementPose.rotation, lerpSpeed));
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void PlaceNote(string noteText, Vector3 position, Quaternion rotation)
+    {
+        GameObject newNote = Instantiate(notePrefab, position, rotation) as GameObject;
+        newNote.GetComponentInChildren<TMP_Text>().text = noteText;
+    }
+}
